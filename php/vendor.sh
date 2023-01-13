5e3b534ea075467bdb55bbde6fd8c062fd068750
Emanuele Fornasier <emanuele.fornasier@outlook.it>
2023-01-13 12:47:11 +0100

    Create vendor.sh

---
+++ php/vendor.sh
@@ -0,0 +1,5 @@
+composer require --dev phpstan/phpstan friendsofphp/php-cs-fixer nunomaduro/larastan:* barryvdh/laravel-ide-helper
+
+php artisan ide-helper:generate
+
+php artisan ide-helper:models
